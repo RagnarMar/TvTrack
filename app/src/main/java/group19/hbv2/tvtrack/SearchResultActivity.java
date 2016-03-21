8c6931613a85213533b3094a6fb58c30fb1bb3c9
agustis <ais3@hi.is>
2016-03-21 00:00:26 +0000

    added a tv info activity with information for each tv series

--- app/src/main/java/group19/hbv2/tvtrack/SearchResultActivity.java
+++ app/src/main/java/group19/hbv2/tvtrack/SearchResultActivity.java
@@ -3,6 +3,7 @@ package group19.hbv2.tvtrack;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.util.Log;
@@ -13,8 +14,10 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import info.movito.themoviedbapi.TmdbApi;
 import info.movito.themoviedbapi.model.tv.TvSeries;
 
 /**
@@ -43,11 +46,33 @@ public class SearchResultActivity extends Activity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 int tvID = (int) view.getTag();
-                //todo: leyfa notanda að ýta á niðurstöðurnar og fá þátta upplýsingaglugga
-                Log.d("=================", Integer.toString(tvID));
+                new GetTvTask().execute(tvID);
             }
         });
 
 
+    }
+
+    private class GetTvTask extends AsyncTask<Integer, Integer, TvSeries> {
+
+        @Override
+        protected TvSeries doInBackground(Integer... params) {
+            int id = params[0];
+            TmdbApi api = new TmdbApi("890f633bdd8840cfdedc2b942c601007");
+            TvSeries result = api.getTvSeries().getSeries(id, "en");
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(TvSeries result) {
+            Intent search = new Intent();
+            search.setClass(getApplicationContext(), TvInfoActivity.class);
+            List<TvSeries> tvlist = new ArrayList<TvSeries>();
+            tvlist.add(result);
+            TvSeriesBundle bundle = new TvSeriesBundle(tvlist);
+            search.putExtra("TvInfo", bundle);
+            startActivity(search);
+            finish();
+        }
     }
 }
--- app/src/main/java/group19/hbv2/tvtrack/TvSeriesAdapter.java
+++ app/src/main/java/group19/hbv2/tvtrack/TvSeriesAdapter.java
@@ -1,14 +1,21 @@
 package group19.hbv2.tvtrack;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.InputStream;
+import java.net.URL;
 import java.util.List;
 
+import info.movito.themoviedbapi.model.Artwork;
 import info.movito.themoviedbapi.model.tv.TvSeries;
 
 /**
@@ -31,6 +38,10 @@ public class TvSeriesAdapter extends ArrayAdapter<TvSeries> {
 
         TextView tvName = (TextView) convertView.findViewById(R.id.tvName);
         tvName.setText(show.getName());
+        //todo: add images and more details
+        //ImageView tvIcon = (ImageView) convertView.findViewById(R.id.tvIcon);
+        //String imgs = show.getPosterPath();
+
 
         convertView.setTag(show.getId());
 
